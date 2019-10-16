dad6260b28f7579366ef50f9fa7ef87a1fc9585e
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-10-16 15:33:35 +0700

    commit-1

---
+++ build/precache-manifest.56d6ac3e5f8dde0aa1db1c98a863d832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f3b31423d46ab87fa9b7660c6615a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf64a4cbaf1181a78832",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7c95b7eeab969cb2bbcd",
+    "url": "/static/js/2.c804eb51.chunk.js"
+  },
+  {
+    "revision": "cf64a4cbaf1181a78832",
+    "url": "/static/js/main.b39afb73.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getData(position.coords.latitude, position.coords.longitude);
+    })
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+  const getData = async (latitude, longitude) => {
+    const API_KEY = "1f38c79d918a202fc06dbd3406c2ba09";
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    console.log(data)
+    setWeather(data)
+    setLoading(false)
+  }
+  // getLocation()
+  console.log(weather)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    isLoading ? <h1>I'm loading...</h1> :
+      (<div className="container-fluid text-white my-auto">
+        <div className="container mx-auto my-4 py-4">
+          <div className="row justify-content-center text-center">
+            <h1 className="col-12 display-4 my-2 py-3 text-success">
+              Awesome Weather App
+          </h1>
+            <h2 className="col-12">{weather && weather.name}</h2>
+            <h3 className="col-12 text-danger">Temperature {weather && weather.main.temp}°C</h3>
+            <img
+              src={`http://openweathermap.org/img/w/${weather &&
+                weather.weather[0].icon}.png`}
+              alt=''
+            />
+            <h3 className="col-12">Weather description: {weather && weather.weather[0].description}</h3>
+          </div>
+        </div>
+      </div>)
   );
 }
 
-export default App;
+export default App
